998c1868586ea2e6b2f97ecce124a90ceee17a06
Titux Metal (MBP) <github@lgdweb.fr>
2022-12-17 18:51:17 +0100

    Updating app.module.ts file; adds auth, user and prisma modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
+import { UserModule } from '~/user'
+
+import { AuthModule } from './auth'
+import { PrismaModule } from './prisma'
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, cache: true })],
-  controllers: []
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    PrismaModule,
+    UserModule,
+    AuthModule
+  ]
 })
 export class AppModule {}
